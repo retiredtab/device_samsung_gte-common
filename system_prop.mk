e8ee2f9d6fcd4612b87944e1ed104570403383f5
retiredtab <68165467+retiredtab@users.noreply.github.com>
2021-01-10 15:15:00 -0700

    Tree for gte-common
    I know SM-T550 is a "A" device and the name for the above is gte-common.  It should be gta-common.  I may change that later.

---
+++ system_prop.mk
@@ -0,0 +1,10 @@
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.snapshot_enabled=1 \
+	persist.radio.snapshot_timer=22 \
+	persist.radio.lte_vrte_ltd=1
+
+# Volume
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.vc_call_vol_steps=15 \
+	ro.config.media_vol_steps=30
